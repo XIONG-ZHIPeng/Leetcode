bde07cc2029e4a466adb8f9740097bdfaa4ff8a3
XIONG Zhipeng <zhipeng@akane.waseda.jp>
2024-11-25 12:57:00 +0900

    optmize

--- 122BestTimetoBuyandSellStock2.py
+++ 122BestTimetoBuyandSellStock2.py
@@ -5,3 +5,18 @@ class Solution:
             if prices[i] > prices[i-1]:
                 profit += prices[i] - prices[i-1]
         return profit
+    
+class Solution:
+    def maxProfit(self, prices):
+        profit = 0
+        buy, sell = prices[0], prices[0]
+        for i in range(1,len(prices)):
+            if prices[i] > sell:
+                sell = prices[i]
+            else:
+                profit += sell - buy
+                buy, sell = prices[i], prices[i]
+        else:
+            profit += sell - buy
+            
+        return profit
